79e0f4bffc92791853bb6616dc633d963556d34b
Prabhat Kumar <prabhat2k15@gmail.com>
2018-09-14 11:16:59 +0530

    test switcher

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+require_once 'vendor/autoload.php';
+
+$s=new Switcher;
+echo $s->sipSwitch();
+?>
---
+++ src/Switcher.php
@@ -0,0 +1,11 @@
+<?php
+
+class Switcher
+{
+    public function sipSwitch()
+    {
+        return 'Sip Switch';
+    }
+}
+
+?>
